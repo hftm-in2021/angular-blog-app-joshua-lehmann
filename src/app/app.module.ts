975df87c6d4dc0c4912ac08494acfe5893b5c8a1
Joshua Lehmann <joshua.le1999@gmail.com>
2023-06-19 12:44:02 +0200

    feat: added lazy loaded home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule } from '@angular/forms';
-import { OverviewComponentComponent } from './components/overview-component/overview-component.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BlogEntryComponent } from './components/blog-entry/blog-entry.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { CoreModule } from './core/core.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
-  declarations: [AppComponent, OverviewComponentComponent, BlogEntryComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    FormsModule,
-    HttpClientModule,
-    MatCardModule,
-    MatButtonModule,
-    MatProgressSpinnerModule,
-  ],
+  declarations: [AppComponent],
+  imports: [CoreModule, AppRoutingModule, CommonModule],
   providers: [],
   bootstrap: [AppComponent],
 })
